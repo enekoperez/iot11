2ae189b78678bc544ba304465a4c4d1f5f8949d3
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-08 20:15:06 +0100

    bluetooth

--- bluetooth.py
+++ bluetooth.py
@@ -1,21 +1,14 @@
+# simple inquiry example
 import bluetooth
 
 
 def main():
-    target_name = "Redmi_K40"
-    target_address = None
 
-    nearby_devices = bluetooth.discover_devices()
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    print("Found {} devices.".format(len(nearby_devices)))
 
-    for bdaddr in nearby_devices:
-        if target_name == bluetooth.lookup_name(bdaddr):
-            target_address = bdaddr
-            break
-
-    if target_address is not None:
-        print("found target bluetooth device with address ", target_address)
-    else:
-        print("could not find target bluetooth device nearby")
+    for addr, name in nearby_devices:
+        print("  {} - {}".format(addr, name))
 
 
 if __name__ == '__main__':
