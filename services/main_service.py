9bb7514cbf5845ee911f4988631c32b5677a09d0
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-08 19:13:21 +0100

    lcd

--- services/main_service.py
+++ services/main_service.py
@@ -20,7 +20,6 @@ class MainService:
 
         print('Running MainService.main ...')
         while self.execution:
-            self.my_sensores.lcd(message2="message 2")
             self.my_sensores.distance(conf_cerca=conf_cerca, conf_lejos=conf_lejos)
             self.my_sensores.light(conf_luz=conf_luz)
             self.my_sensores.temp_and_humi(conf_temp=conf_temp, conf_humi=conf_humi)
