c24096c1d00beda233cb9dc0884150e5a72438f3
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-09 21:08:55 +0100

    buzzer2

--- pruebas.py
+++ pruebas.py
@@ -21,7 +21,6 @@ def main():
 
 # !/usr/bin/env python
 
-import time
 from grove.grove_ryb_led_button import GroveLedButton
 
 
@@ -36,8 +35,26 @@ def led():
         time.sleep(1)
 
 
-if __name__ == '__main__':
-    led()
+# !/usr/bin/env python
+import time
+from mraa import getGpioLookup
+from upm import pyupm_buzzer as upmBuzzer
+
+
+def buzzer2():
+    # Grove - Buzzer connected to PWM port
+    buzzer = upmBuzzer.Buzzer(getGpioLookup('GPIO12'))
+
+    CHORDS = [upmBuzzer.BUZZER_DO, upmBuzzer.BUZZER_RE, upmBuzzer.BUZZER_MI,
+              upmBuzzer.BUZZER_FA, upmBuzzer.BUZZER_SOL, upmBuzzer.BUZZER_LA,
+              upmBuzzer.BUZZER_SI]
+    for i in range(0, len(CHORDS)):
+        buzzer.playSound(CHORDS[i], 500000)
+        time.sleep(0.1)
+
+    del buzzer
+    print('application exiting...')
+
 
 if __name__ == '__main__':
-    main()
+    buzzer2()
