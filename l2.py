acd8d0a28e272005aa6970eb6b51ab3ef5e36f67
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-14 22:49:19 +0100

    l2 again

--- l2.py
+++ l2.py
@@ -33,7 +33,6 @@ def main():
     button.on_event = on_event
 
     # only add the detection call once!
-    gpio.add_event_detect()
     gpio.add_event_detect(self.pin, RPi.GPIO.BOTH, self._on_event)
 
     while True:
