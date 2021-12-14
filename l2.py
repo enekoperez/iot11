fdc7a3c03267c3fa7143f9822b5798d8df3338cc
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-14 22:35:54 +0100

    l2 again

--- l2.py
+++ l2.py
@@ -5,6 +5,7 @@ import time
 from grove.button import Button
 from grove.grove_ryb_led_button import GroveLedButton
 
+import RPi.GPIO as gpio
 
 # from mraa import getGpioLookup
 # from upm import pyupm_buzzer as upmBuzzer
@@ -18,8 +19,25 @@ def main():
     # Grove - Buzzer connected to PWM port
     # buzzer = upmBuzzer.Buzzer(getGpioLookup('GPIO12'))
 
-    button.led.light(True)
+    def on_event(index, event, tm):
+        if event & Button.EV_SINGLE_CLICK:
+            print('single click')
+            button.led.light(True)
             # buzzer.playSound(upmBuzzer.BUZZER_DO, 500000)
+
+        elif event & Button.EV_LONG_PRESS:
+            print('long press')
+            button.led.light(False)
+            # buzzer.playSound(upmBuzzer.BUZZER_DO, 1000000)
+
+    button.on_event = on_event
+
+    while True:
+        try:
+            # do any other processing, while waiting for the edge detection
+            time.sleep(1)  # sleep 1 sec
+        finally:
+            gpio.cleanup()
 
 
 if __name__ == '__main__':
