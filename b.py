168e2c2722907b29806fca5d8c54890a13f1071a
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-16 18:16:40 +0100

    bluetooth

--- b.py
+++ b.py
@@ -1,6 +1,40 @@
+#!/usr/bin/python
 import bluetooth
-print("performing inquiry...")
-nearby_devices = bluetooth.discover_devices(lookup_names = True)
-print("found %d devices" % len(nearby_devices))
-for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
+import time
+
+HectorIN = 0
+HectorOUT = 0
+
+print
+"In/Out Board"
+
+while True:
+    print
+    "Buscando " + time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
+result = bluetooth.lookup_name('A0:18:28:59:XX:XX', timeout=5)
+if (result != None):
+    print
+    "Hector: in"
+if HectorIN == 0:
+    HectorIN = HectorIN + 1
+import subprocess
+
+subprocess.call(['bash', '/home/pi/alarma_apaga.sh'])
+subprocess.call(['bash', '/home/pi/textoAvoz.sh', 'Bienvenido a casa Hector'])
+HectorOUT = 0
+time.sleep(300)
+else:
+print
+"Hector: out"
+HectorIN = 0
+HectorOUT = HectorOUT + 1
+print
+"Hector IN:"
+print
+HectorIN
+print
+"HectorOUT"
+print
+HectorOUT
+print
+"------"
