31356cde788fd95b130920d6ac5e754063c9b7b1
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-22 18:44:45 +0100

    bluetooth

--- b.py
+++ b.py
@@ -12,5 +12,15 @@ for bdaddr in nearby_devices:
 
 if target_address is not None:
     print ("found target bluetooth device with address ", target_address)
+
+    port = 1
+
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((target_address, port))
+
+    sock.send("hello!!")
+
+    sock.close()
+
 else:
     print ("could not find target bluetooth device nearby")
