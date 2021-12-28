6bb36f3beced4cd8fac3343e0797199f0a5c7da6
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2021-12-28 18:24:22 +0100

    temp and hum api

--- api/iot_api.py
+++ api/iot_api.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, jsonify
 
 from services.main_service import MainService
+from services.sensores import Sensores
 
 iotApi = Blueprint('iotApi', __name__)
 
 my_main_service = MainService()
+my_sensores = Sensores()
 
 
 @iotApi.route('execute', methods=['GET'])
@@ -18,3 +20,11 @@ def execute():
 def ender():
     my_main_service.ender()
     return jsonify(message='ender done'), 200
+
+@iotApi.route('temp-and-humi', methods=['GET'])
+def temp_and_humi():
+    if request.is_json:
+        conf_temp = request.json['conf_temp']
+        conf_humi = request.json['conf_humi']
+        response = my_sensores.temp_and_humi(conf_temp=conf_temp, conf_humi=conf_humi)
+        return jsonify(response), 200
