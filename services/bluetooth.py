95427dc2e6c875fc8669f0f0acc66e9eb7995f7e
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2022-01-09 18:11:53 +0100

    edu bluet

--- services/bluetooth.py
+++ services/bluetooth.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def running_commands():
-	pro= subprocess.run('/iot11/bt.sh')
+	pro= subprocess.run('/home/iot11/bt.sh')
 	print(pro.returncode)
 	if int(pro.returncode)==0:
 		print("pass")
