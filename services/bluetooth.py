6f317e1d6969cab21489a9f704dc3ba4333208d2
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2022-01-09 18:19:02 +0100

    edu bluet

--- services/bluetooth.py
+++ services/bluetooth.py
@@ -1,11 +1,23 @@
 import subprocess
+from time import sleep
+
+import serial
+
 
 def running_commands():
-	pro= subprocess.run('/home/pi/iot11/bt.sh')
-	print(pro.returncode)
-	if int(pro.returncode)==0:
-		print("pass")
-	else:
-		print("fail")
+    pro = subprocess.run('/home/pi/iot11/bt.sh')
+    print(pro.returncode)
+    if int(pro.returncode) == 0:
+        print("pass")
+    else:
+        print("fail")
+
 
 running_commands()
+sleep(10)
+
+data = str(0)
+b = bytes(data, 'utf-8')
+ser = serial.Serial('/dev/rfcomm0')
+ser.isOpen()
+ser.write(b)
