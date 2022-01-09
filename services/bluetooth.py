1683fde2dd30c6e4714a8d6db6beab11ee03aedf
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2022-01-09 19:37:19 +0100

    edu bluet

--- services/bluetooth.py
+++ services/bluetooth.py
@@ -14,7 +14,7 @@ def running_commands():
 
 
 def send_b(received_data):
-    received_data = str(received_data)
+    received_data = str(received_data + '\r\n')
     to_send_data = received_data.encode('utf-8')
     ser = serial.Serial('/dev/rfcomm0')
     ser.isOpen()
