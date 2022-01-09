14d6aea204df2568f1e90e9ffa3024cc13dbbbad
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2022-01-09 20:05:56 +0100

    edu bluet

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def get_flask_app() -> app.Flask:
     flask_app = Flask(__name__)
     flask_app.config.from_object("config.Config")
     routes.init_routes(flask_app)
-    subprocess.call("bt.sh &", shell=True)
+    subprocess.call("./bt.sh &", shell=True)
     return flask_app
 
 
