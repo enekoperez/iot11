fafa540db7b49daa6a9439a417234f8ad6934797
jokinme99 <jokinme99@opendeusto.es>
2022-01-10 22:55:34 +0100

    apikey update

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     CORLYSIS_URL = "https://corlysis.com:8086/write"
     CORLYSIS_DATABASE = "iot1"
     CORLYSIS_USER = "token"
-    CORLYSIS_PASSWORD = "41b94ee8b7c2df9370bceb2b288a5ee2"
+    CORLYSIS_PASSWORD = "12a445bce90b255fcedf510df6ae68e1"
 
     # SENSORES
     BUZZER = 26
